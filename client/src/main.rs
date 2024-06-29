6576bab11be89ea3ab8a95206133fc5f59c3ef40
anthony-major <148173066+anthony-major@users.noreply.github.com>
2024-06-29 12:28:27 -0400

    Added broadcasting
    Extended the server to broadcast messages received from clients to all clients. Messages are still just plain strings, no protocol or anything, but basic broadcasting is working.
    
    Extended the basic client as well, to read and send messages from stdin and read messages from its TCP stream concurrently using tokio, so that a client can see broadcasted messages while inputting their own.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,33 +1,48 @@
-use std::io::{self, Write};
-use std::net::TcpStream;
+use tokio::io::{self, BufReader, BufWriter, AsyncBufReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
 use clap::Parser;
 
-fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     let args = Args::parse();
     let addr = format!("{}:{}", args.address, args.port);
 
     println!("Connecting to {}...", addr);
-    let mut stream = TcpStream::connect(addr)?;
+    let mut stream = TcpStream::connect(addr).await?;
     println!("Connected.");
 
-    let mut input = String::new();
+    let (read_stream, write_stream) = stream.split();
+
+    let mut buf_read_stream = BufReader::new(read_stream);
+    let mut buf_write_stream = BufWriter::new(write_stream);
+    let mut stream_input = String::new();
+
+    let mut buf_reader = BufReader::new(io::stdin());
+    let mut user_input = String::new();
 
     loop {
-        print!(">");
-        io::stdout().flush()?;
-        io::stdin().read_line(&mut input)?;
-        input = input.replace("\\n", "\n");
+        tokio::select! {
+            _ = buf_reader.read_line(&mut user_input) => {
+                user_input = user_input.replace("\\n", "\n");
 
-        if input.trim_end() == "quit" {
-            break;
+                if user_input.trim_end() == "quit" {
+                    break;
+                }
+
+                println!("Sending message:\n{}", user_input);
+                buf_write_stream.write(user_input.as_bytes()).await?;
+                buf_write_stream.flush().await?;
+                println!("Sent.");
+
+                user_input.clear();
+            }
+            _ = buf_read_stream.read_line(&mut stream_input) => {
+                println!("Read message:\n{}", stream_input);
+
+                stream_input.clear();
+            }
         }
-
-        println!("Sending message:\n{}", input);
-        stream.write(input.as_bytes())?;
-        println!("Sent.");
-
-        input.clear();
     }
 
     Ok(())
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,9 @@
 use std::net::SocketAddr;
 
-use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::signal;
+use tokio::sync::broadcast::{channel, Receiver, Sender};
 
 use clap::Parser;
 
@@ -15,11 +16,13 @@ async fn main() -> io::Result<()> {
     println!("Listening on port {}...", args.port);
     println!("Press ctrl+c to exit.");
 
+    let (tx, _) = channel::<String>(16);
+
     loop {
         tokio::select! {
             future = listener.accept() => {
                 let (stream, addr) = future?;
-                tokio::spawn(handle_client(stream, addr));
+                tokio::spawn(handle_client(stream, addr, tx.clone(), tx.subscribe()));
             },
             _ = signal::ctrl_c() => {
                 break;
@@ -31,28 +34,47 @@ async fn main() -> io::Result<()> {
     Ok(())
 }
 
-async fn handle_client(stream: TcpStream, addr: SocketAddr) -> io::Result<()> {
+async fn handle_client(
+    stream: TcpStream,
+    addr: SocketAddr,
+    tx: Sender<String>,
+    mut rx: Receiver<String>,
+) -> io::Result<()> {
     println!("{} connected.", addr);
 
-    let (read_stream, _write_stream) = io::split(stream);
+    let (read_stream, mut write_stream) = io::split(stream);
 
     let mut lines = BufReader::new(read_stream).lines();
 
     loop {
-        match lines.next_line().await {
-            Ok(line) => {
+        tokio::select! {
+            line = lines.next_line() => {
                 match line {
-                    Some(line) => {
-                        println!("[{}:{}] {}", addr.ip(), addr.port(), line);
+                    Ok(line) => {
+                        match line {
+                            Some(line) => {
+                                println!("[{}:{}] {}", addr.ip(), addr.port(), line);
+                                tx.send(line).expect("Failed to send through tx.");
+                            }
+                            _ => {
+                                break;
+                            }
+                        }
                     }
-                    _ => {
-                        break;
+                    Err(e) => {
+                        println!("{} forcefully disconnected.", addr);
+                        return Err(e);
                     }
                 }
             }
-            Err(e) => {
-                println!("{} forcefully disconnected.", addr);
-                return Err(e);
+            message = rx.recv() => {
+                let mut message = message.expect("Failed to read from rx.");
+                message.push('\n');
+
+                println!("Broadcasting to {}...", addr);
+                write_stream.write(message.as_bytes()).await.expect("Failed to write to stream.");
+                write_stream.flush().await.unwrap();
+                println!("Broadcasted to {}.", addr);
             }
         }
     }
