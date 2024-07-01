c02cdd00878412e050cf6d39c3db04fbea841585
anthony-major <148173066+anthony-major@users.noreply.github.com>
2024-07-01 13:41:24 -0400

    More testing
    - Rewrote client program to test multiple clients interacting with the server at the same time. The client program spawns tasks in a loop; each task connects to the server, then sends and reads a series of messages concurrently.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,51 +1,74 @@
-use tokio::io::{self, BufReader, BufWriter, AsyncBufReadExt, AsyncWriteExt};
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio::net::TcpStream;
+use tokio::signal;
+
+use serde::{Deserialize, Serialize};
 
 use clap::Parser;
+use tokio::task::JoinHandle;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let args = Args::parse();
     let addr = format!("{}:{}", args.address, args.port);
 
+    let mut handles: Vec<JoinHandle<Result<(), io::Error>>> = Vec::new();
+
+    for i in 0..2 {
+        let handle = tokio::spawn(run(addr.clone(), i.to_string()));
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await?.unwrap();
+    }
+
+    Ok(())
+}
+
+async fn run(addr: String, username: String) -> io::Result<()> {
     println!("Connecting to {}...", addr);
-    let mut stream = TcpStream::connect(addr).await?;
+    let stream = TcpStream::connect(addr).await?;
     println!("Connected.");
 
-    let (read_stream, write_stream) = stream.split();
+    let (read_stream, write_stream) = io::split(stream);
 
     let mut buf_read_stream = BufReader::new(read_stream);
     let mut buf_write_stream = BufWriter::new(write_stream);
     let mut stream_input = Vec::<u8>::new();
 
-    let mut buf_reader = BufReader::new(io::stdin());
-    let mut user_input = String::new();
+    let handle1 = tokio::spawn(async move {
+        for i in 0..16 {
+            let message = UserMessage {
+                username: username.clone(),
+                content: format!("{} {}", username, i),
+            };
+            let mut message = serde_json::to_string(&message).unwrap();
+            message.push('\0');
 
-    loop {
-        tokio::select! {
-            _ = buf_reader.read_line(&mut user_input) => {
-                if user_input.trim_end() == "quit" {
+            println!("{} sending message {}...", username, i);
+            buf_write_stream.write(message.as_bytes()).await.unwrap();
+            buf_write_stream.flush().await.unwrap();
+            println!("{} Sent {}.\n", username, i);
+        }
+    });
+
+    let handle2 = tokio::spawn(async move {
+        loop {
+            tokio::select! {
+                _ = signal::ctrl_c() => {
                     break;
                 }
-
-                user_input = user_input.replace("\\n", "\n");
-                user_input.pop();
-                user_input.push('\0');
-
-                println!("Sending message:\n{}", user_input);
-                buf_write_stream.write(user_input.as_bytes()).await?;
-                buf_write_stream.flush().await?;
-                println!("Sent.");
-
-                user_input.clear();
-            }
-            _ = buf_read_stream.read_until(b'\0', &mut stream_input) => {
-                println!("Read message:\n{}", String::from_utf8(stream_input).unwrap());
-
-                stream_input = Vec::<u8>::new();
+                _ = buf_read_stream.read_until(b'\0', &mut stream_input) => {
+                    println!("Read message:\n{}\n", String::from_utf8(stream_input).unwrap());
+                    stream_input = Vec::<u8>::new();
+                }
             }
         }
-    }
+    });
+
+    handle1.await?;
+    handle2.await?;
 
     Ok(())
 }
@@ -58,3 +81,36 @@ struct Args {
     #[arg(short, long, default_value_t = 9000)]
     port: u16,
 }
+
+#[derive(Serialize, Deserialize)]
+struct UserMessage {
+    username: String,
+    content: String,
+}
+
+// Old loop for getting and sending input from stdin
+// loop {
+//     tokio::select! {
+//         // _ = buf_reader.read_line(&mut user_input) => {
+//         //     if user_input.trim_end() == "quit" {
+//         //         break;
+//         //     }
+
+//         //     user_input = user_input.replace("\\n", "\n");
+//         //     user_input.pop();
+//         //     user_input.push('\0');
+
+//         //     println!("Sending message:\n{}", user_input);
+//         //     buf_write_stream.write(user_input.as_bytes()).await?;
+//         //     buf_write_stream.flush().await?;
+//         //     println!("Sent.");
+
+//         //     user_input.clear();
+//         // }
+//         _ = buf_read_stream.read_until(b'\0', &mut stream_input) => {
+//             println!("Read message:\n{}", String::from_utf8(stream_input).unwrap());
+
+//             stream_input = Vec::<u8>::new();
+//         }
+//     }
+// }
